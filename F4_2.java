84b6135e290d4e1d18cfe2721b7e7c88bf5fa8b8
BenjaminFrankln <maksyork@gmail.com>
2015-10-20 13:32:18 +0300

    Create F4_2.java

---
+++ F4_2.java
@@ -0,0 +1,18 @@
+package lesson_1;
+
+public class F4_2 {
+
+public static void main(String[] args) {
+		
+		String text = "Everybody hello! ";
+		String text1 = "It's a beautiful day! " + "So, don't worry =) ";
+		String text2 = "Today is 20.10.2015 ";
+		String text3 = "!!                   !!";
+		
+
+		
+		System.out.print(text+text1+text2+text3);
+		
+	}
+
+}
